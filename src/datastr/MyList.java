93bb2480842d674e2baba231796e415e3c1f904c
krisnawa00 <kristersrudzitiss@gmail.com>
2025-02-15 15:28:51 +0200

    update

--- src/datastr/MyList.java
+++ src/datastr/MyList.java
@@ -2,4 +2,15 @@ package datastr;
 
 public class MyList {
 
+	private char[]list;
+	private final int DEFAULT_SIZE =10;
+	private int size = DEFAULT_SIZE;
+	private int counter = 0;
+	
+	public MyList() {
+		list = new char [size];
+	}
+	public MyList (int inputSize) {
+		
+	}
 }
