ee73d8fc86153ec13e78c9cd2ea0bdbd769df471
EmreOren <m.emreoren@gmail.com>
2025-02-20 13:23:07 +0200

    kautkas

--- src/datastr/MyList.java
+++ src/datastr/MyList.java
@@ -1,67 +1,112 @@
 package datastr;
 
 public class MyList {
-
-	private char[]list;
-	private final int DEFAULT_SIZE =10;
+	//mainīgie
+	private char[] list;
+	private final int DEFAULT_SIZE = 10;
 	private int size = DEFAULT_SIZE;
 	private int counter = 0;
 	
+	//bezargumenta konstruktors
 	public MyList() {
-		list = new char [size];
+		list = new char[size];
 	}
-	public MyList (int inputSize) {
-		if (inputSize > 0) {
+	
+	//argumenta konstruktors
+	public MyList(int inputSize) {
+		if(inputSize > 0) {
 			size = inputSize;
 		}
-		list = new char [size];
+		
+		list = new char[size];	
 	}
+	
 	public boolean isFull()
 	{
-		return (counter == size) ? true : false;
+		//garais if-else
+		/*if(counter == size) {
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		*/
+		
+		//īso if-else
+		//return (counter == size) ? true : false;
+		
+		
+		//īsā forma priekš true/false
+		return (counter == size);
 		
 	}
-	public boolean isEmpty()
-	{
+	
+	public boolean isEmpty() {
 		return (counter == 0);
-		
 	}
-	public int HowManyElements()
-	{
+	
+	public int howManyElements() {
 		return counter;
-		
 	}
+	
 	private void resize() {
-		size = (counter<100) ? size*2 : (int) (size * 1.5);
-		char[]listNew = new char [size];
-		for (int i = 0; i < counter;i++) {
-			listNew[i] = list [i];
+		//garais if-else
+		/*if(counter < 100) {
+			size = size * 2;
+		}
+		else
+		{
+			size = (int)(size * 1.5);
+		}
+		*/
+		//īsais if-else
+		size = (counter < 100) ? size * 2 : (int)(size * 1.5);
+		char[] listNew = new char[size];
+		
+		for(int i = 0; i < counter; i++) {
+			listNew[i] = list[i];
 		}
 		list = listNew;
-		System.gc();
+		System.gc();//izsaucam atkritumu savācēju, kas izdzēsīs mazā masīva elementus
 		
 	}
+	
 	public void add(char element) {
 		if(isFull()) {
 			resize();
 		}
-		list[counter++] = element;
-		counter++;
-	}
-	public void add(char element, int index) {
 		
-		if (index < 0 || index > counter) {
-			//TODO izmest iznemumu
+		//garais pieraksts
+		list[counter] = element;
+		counter++;
+		
+		//īsais pieraksts
+		//list[counter++] = element;
+	}
+	
+	public void add(char element, int index) {
+		if(index < 0 || index > counter)
+		{
+			//TODO izmest izņēmumu, jo index nav pareizs
 		}
-		else {
+		else
+		{
 			if(isFull()) {
 				resize();
 			}
-			for (int i = counter; i > index; i--) {
+			
+			for(int i = counter; i >= index; i--) {
 				list[i+1] = list[i];
 			}
 			list[index] = element;
 			counter++;
+			
 		}
 	}
+	
+	
+	
+	
+
 }
